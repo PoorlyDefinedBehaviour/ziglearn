03be4afd23143a507044e4f3aea463cdd2dc2b54
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-03-11 12:40:03 -0300

    switch

---
+++ chapter_1/switch.zig
@@ -0,0 +1,29 @@
+const expect = @import("std").testing.expect;
+
+test "switch statement" {
+    var x: i8 = 10;
+
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+
+    try expect(x == 1);
+}
+
+test "switch expression" {
+    var x: i8 = 10;
+
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x
+    };
+
+    try expect(x == 1);
+}
