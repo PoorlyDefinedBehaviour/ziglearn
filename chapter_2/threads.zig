81a4b7a2b551e0a642d3fed9b7af5ae138caf97a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-03-11 17:07:20 -0300

    threads

---
+++ chapter_2/threads.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+var tick: isize = 0;
+
+fn ticker(step: u8) void {
+    while(true) {
+        std.time.sleep(1 * std.time.ns_per_s);
+        tick += @as(isize, step);
+    }
+}
+
+test "threading" {
+    var thread = try std.Thread.spawn(.{}, ticker, .{@as(u8, 1)});
+    _ = thread;
+    try expect(tick == 0);
+    std.time.sleep(3 * std.time.ns_per_s / 2);
+    try expect(tick == 1);
+}
