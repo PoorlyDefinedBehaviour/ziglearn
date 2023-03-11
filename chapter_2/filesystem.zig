be33d0c8c8e0760877602a45116c2850a976dcfb
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-03-11 16:15:59 -0300

    filesystem

---
+++ chapter_2/filesystem.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "createFile, write, seekTo, read" {
+    const file = try std.fs.cwd().createFile("junk_file.txt", .{.read = true});
+    defer file.close();
+
+    const bytes_written = try file.writeAll("Hello File!");
+    _ = bytes_written;
+
+    var buffer: [100]u8 = undefined;
+    try file.seekTo(0);
+    const bytes_read = try file.readAll(&buffer);
+
+    try expect(std.mem.eql(u8, buffer[0..bytes_read], "Hello File!"));
+}
+
+test "file stat" {
+    const file = try std.fs.cwd().createFile("junk_file2.txt", .{.read = true});
+    defer file.close();
+    
+    const stat = try file.stat();
+
+    try expect(stat.size == 0);
+    try expect(stat.kind == .File);
+    try expect(stat.ctime <= std.time.nanoTimestamp());
+    try expect(stat.mtime <= std.time.nanoTimestamp());
+    try expect(stat.atime <= std.time.nanoTimestamp());
+}
+
+test "make dir" {
+    try std.fs.cwd().makeDir("test-tmp");
+
+    const dir = try std.fs.cwd().openDir("test-tmp", .{});
+    defer std.fs.cwd().deleteTree("test-tmp") catch unreachable;
+
+    _ = try dir.createFile("x", .{});
+    _ = try dir.createFile("y", .{});
+    _ = try dir.createFile("z", .{});
+
+    const iteratableDir = try std.fs.cwd().openIterableDir("test-tmp", .{});
+
+    var file_count: usize = 0;
+    var iter = iteratableDir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .File) {
+            file_count += 1;
+        }
+    }
+
+    try expect(file_count == 3);
+}
