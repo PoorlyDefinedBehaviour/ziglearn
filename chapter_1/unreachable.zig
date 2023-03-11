d78790be11928ab986fbd21a3a97024930be9ba4
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-03-11 12:45:26 -0300

    unreachable

---
+++ chapter_1/unreachable.zig
@@ -0,0 +1,20 @@
+const expect = @import("std").testing.expect;
+
+test "unreachable" {
+    const x: i32 = 1;
+    const y: u32 = if (x == 2) 5 else unreachable;
+    _ = y;
+}
+
+fn asciiToUpper(x: u8) u8 {
+    return switch (x) {
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        else => unreachable
+    };
+}
+
+test "unreachable switch" {
+    try expect(asciiToUpper('a') == 'A');
+    try expect(asciiToUpper('A') == 'A');
+}
